65247175d0cb22e2e9d9fa93a3c74810dd0fa6cd
Mehmet Can Farsak <32980057+mehmetcanfarsak@users.noreply.github.com>
2022-10-14 22:12:40 +0300

    Update setup.py
    small changes

--- setup.py
+++ setup.py
@@ -8,22 +8,18 @@ setup(
     author = 'AldimGeldi',
     author_email = 'teknik@aldimgeldi.com',
     url = 'https://github.com/AldimGeldi/serverless_cache',
-    download_url = 'https://github.com/AldimGeldi/reponame/archive/v_01.tar.gz',    # I explain this later on
-    keywords = ['serverless', 'cache', 'serverless-db','serverless-cache'],   # Keywords that define your package best
-    install_requires=[            # I get to this in a second
+    download_url = 'https://github.com/AldimGeldi/reponame/archive/v_01.tar.gz',   
+    keywords = ['serverless', 'cache', 'serverless-db','serverless-cache'],   
+    install_requires=[            
         'simplejson',
         'boto3',
     ],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Development Status :: 5 - Production/Stable',      
+        'Intended Audience :: Developers',      
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',   # Again, pick a license
-        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',   
+        'Programming Language :: Python :: 3',      
+
     ],
 )
